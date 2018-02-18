91d27f347cd857a97df55eb290e812fdf07a4d45
Zgen <zgenstudio@gmail.com>
2018-02-18 12:22:33 +0200

    home half

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".sliderHome").slick({
+		infinite: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 3,
+		dots: true,
+		dragable: false,
+		arrows: false
+	});
+});
