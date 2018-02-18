268fad116dbcabe5157c487bfd18565158555c53
noname-username <32517547+noname-username@users.noreply.github.com>
2018-02-18 23:17:40 +0300

    contacts + master_works + service (unfinished)

---
+++ js/18.02.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.serviceSlider').slick({
+        infinite: true,
+        arrows: true,
+        prevArrow: '<div class="serviceSlider__prev"></div>',
+        nextArrow: '<div class="serviceSlider__next"></div>',
+        draggable: true,
+        autoplay: true
+    });
+})
